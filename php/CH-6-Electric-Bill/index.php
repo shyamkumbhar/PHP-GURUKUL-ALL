100416395fd5ae2a4fa6f75956ac97ba6557c4a1
shyamkumbhar <shyamkumbhar509@gmail.com>
2021-11-14 23:53:14 +0530

    PHP
    add php file

---
+++ php/CH-6-Electric-Bill/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>login Funtonality</title>
+</head>
+<body>
+    <h2> Login Form</h2>
+    <form action="check.php" method="post">
+          <p>Enter Value : <input type="number" name="number"></p>
+          <p> <input type="submit" value="calculate"></p>
+    </form>
+</body>
+</html>
