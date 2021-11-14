b0525ce29e7eb3f39609b97a1e22249edfdad129
shyamkumbhar <shyamkumbhar509@gmail.com>
2021-11-14 23:57:03 +0530

    php
    dd

---
+++ php/include-Require/content.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+   /*  include('header.php'); */
+    require('header.php');
+
+    ?>
+
+    <p>this code is of content file</p>
+</body>
+
+</html>
